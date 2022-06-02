437b2f53beddf82cb303da07caae03081c1c4fb9
hclee <heechul4296@gmail.com>
2022-06-03 01:56:27 +0900

    디자인 패턴 - 싱글톤

---
+++ src/main/java/study/spring/introduction/design/Main.java
@@ -0,0 +1,20 @@
+package study.spring.introduction.design;
+
+import study.spring.introduction.design.singleton.AClazz;
+import study.spring.introduction.design.singleton.BClazz;
+import study.spring.introduction.design.singleton.SocketClient;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        AClazz aClazz = new AClazz();
+        BClazz bClazz = new BClazz();
+
+        SocketClient aClient = aClazz.getSocketClient();
+        SocketClient bClient = bClazz.getSocketClient();
+
+        System.out.println("두개의 객체가 동일한가?");
+        System.out.println(aClient.equals(bClient));
+    }
+}
---
+++ src/main/java/study/spring/introduction/design/singleton/SocketClient.java
@@ -0,0 +1,22 @@
+package study.spring.introduction.design.singleton;
+
+public class SocketClient {
+
+    private static SocketClient socketClient = null;
+
+    private SocketClient() {
+
+    }
+
+    public static SocketClient getInstance() {
+        if (socketClient == null) {
+            socketClient = new SocketClient();
+        }
+        return socketClient;
+    }
+
+    public void connect() {
+        System.out.println("connect");
+    }
+
+}
