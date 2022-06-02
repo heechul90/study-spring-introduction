9c2114e76cd96e9b90749c4df4b6d19c2f698193
hclee <heechul4296@gmail.com>
2022-06-03 01:11:56 +0900

    upload 초기소스

---
+++ src/main/java/study/spring/introduction/IntroductionApplication.java
@@ -0,0 +1,13 @@
+package study.spring.introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/spring/introduction/IntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
